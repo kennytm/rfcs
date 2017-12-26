0c87c39b3922311f2baade5f88dd454b034a1fed
kennytm <kennytm@gmail.com>
2017-12-26 17:06:09 +0800

    Modify rustbook generator to support directory-style RFC.

--- generate-book.sh
+++ generate-book.sh
@@ -2,16 +2,23 @@
 
 set -e
 
-if [ ! -d src ]; then
-    mkdir src
-fi
+mkdir -p src
+cp -r text/* src
 
-echo "[Introduction](introduction.md)\n" > src/SUMMARY.md
+echo "[Introduction](introduction.md)" > src/SUMMARY.md
+echo "" >> src/SUMMARY.md
 
-for f in $(ls text/* | sort)
+for fn in $(ls text | sort)
 do
-    echo "- [$(basename $f ".md")]($(basename $f))" >> src/SUMMARY.md
-    cp $f src
+    f="text/$fn"
+    if [ -f "$f/README.md" ]; then
+        echo "- [$(basename $fn "/")]($fn/README.md)" >> src/SUMMARY.md
+        for subfn in $(ls -1 $f/*-*.md | sort); do
+            echo "    - [$(basename "$subfn" ".md" | cut -d- -f 2-)]($fn/$(basename "$subfn"))" >> src/SUMMARY.md
+        done
+    elif [ -f "$f" ]; then
+        echo "- [$(basename $fn ".md")]($fn)" >> src/SUMMARY.md
+    fi
 done
 
 cp README.md src/introduction.md
